b1a43b9358460ddf41917753217078817c9f5cb8
HelloJunWei <guoleo2008@yahoo.com.tw>
2020-05-18 13:48:36 +0800

    add plugin-proposal-decorators and example

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log('init')
+@superhero
+class MySuperHero {};
+
+function superhero (target) {
+  target.superhero = true;
+  target.power = 'flight';
+};
+
+console.log(MySuperHero.superhero);
+// true
+console.log(MySuperHero.power);
+// flight
+
+class Cat {
+  @readonly
+  meow () {
+    console.log('meow');
+  }
+}
+
+function readonly (target, key, descriptor) {
+  descriptor.writable = false;
+  return descriptor;
+}
+
+var cat = new Cat();
+cat.meow();
+// meow
+cat.meow = () => { console.log('I want lasagne')};
+// 沒辦法更換
+
+cat.meow();
+// meow
